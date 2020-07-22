8f9d2197a411f2fd8f5400d798726d04194ff946
venom <venom@DESKTOP-VI6EVSE>
2020-07-22 00:28:11 -0500

    Cambios del código.

---
+++ Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsolaTerceroA
+{
+    class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public string Saludar(string Nombre)
+        {
+            return "Hola " + Nombre + " me llamo" + this.Nombre;
+        }
+
+        public void WriteLine(string dato)
+        {
+
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsolaTerceroA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine(args[0]);
+            }
+            Console.WriteLine("Ingrese un dato!!!");
+            string datosLeidos = Console.ReadLine();
+            Console.WriteLine($"Usted ingresó lo siguiente {datosLeidos}");
+            Console.ReadKey();
+        }
+    }
+}
